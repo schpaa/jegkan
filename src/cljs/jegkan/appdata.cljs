65ca7b10d73fdc54c44e0572cfa9e76d3e7a1182
Chris Patrick Schreiner <schpaa.coder@gmail.com>
2022-02-16 22:13:25 +0100

    init

---
+++ src/cljs/jegkan/appdata.cljs
@@ -0,0 +1,56 @@
+(ns jegkan.appdata
+  (:require [re-frame.core :refer [reg-sub]]
+            [cljs.reader :refer [read-string]]
+            [schpaa.state]))
+
+(def routes
+  [["/" {:name :r.forsiden :header "NRPK Booking" :subheader "Forsiden"}]
+   ["/batlist" {:name :r.boatlist :header "Oversikt over båter" :subheader "Forsiden"}]
+   ["/ny" {:name :r.new-booking :header "Ny booking" :subheader "Forsiden"}]
+   ["/debug" {:name :r.debug :header "Feilsøking" :subheader "Baksiden"}]
+   ["/turlogg" {:name :r.logg :header "Min logg" :subheader "Baksiden"}]
+   ["/om-meg" {:name :r.user :header "Om meg" :subheader "Baksiden"}]
+
+   ;;todo these?
+   ["/init" {:name :r.init :header "Init" :subheader "Booking"}]
+   ["/innhold" {:name :r.content :header "Innhold" :subheader "Booking"}]
+   ["/siste" {:name :r.last-booking :header "Siste booking" :subheader "Booking"}]
+   ["/baksiden" {:name :r.back :header "Baksiden" :subheader "Booking"}]
+   ["/debug2" {:name :r.debug2 :header "Feilsøking2" :subheader "Booking"}]
+   ["/blog" {:name :r.blog :header "Blogg" :subheader "Booking"}]
+   ["/not-found" {:name :r.page-not-found :header ":r.page-not-found" :subheader ":r.page-not-found"}]])
+
+
+;region initial-state
+
+(def start-db
+  {:version  "3.0.23"
+   :app/name "booking"
+   :tab      :cloud})
+
+(defn initialize
+      [db ls-key]
+      (conj db
+            (into (sorted-map)
+                  (some->> (.getItem js/localStorage ls-key)
+                           (read-string)))))
+
+(def initial-db
+  (initialize start-db schpaa.state/ls-key))
+
+;endregion
+
+;region firebase
+
+(def booking-firebaseconfig
+  ;eykt-22
+  {:apiKey            "AIzaSyD2iU_6-1pre0lbYERk8jDdzMgrs-3QVlY",
+   :authDomain        "nrpk-vakt.firebaseapp.com",
+   :databaseURL       "https://nrpk-vakt.firebaseio.com",
+   :projectId         "nrpk-vakt",
+   :storageBucket     "nrpk-vakt.appspot.com",
+   :messagingSenderId "461903557643",
+   :appId             "1:461903557643:web:f51109ad017ed61a6c430e",
+   :measurementId     "G-FYD8HLS5C1"})
+
+;endregion
---
+++ src/cljs/jegkan/core.cljs
@@ -0,0 +1,32 @@
+(ns jegkan.core
+  (:require
+    [re-frame.core :as rf]
+    [kee-frame.core :as k]
+    ;; passively registers the fsm
+    [re-statecharts.core :as rs]
+    [jegkan.appdata :as app-data]))
+    ;[booking.spa :as spa]
+    ;[booking.fsm-model]))
+    ;[schpaa.darkmode]
+    ;[db.core :as db]
+    ;[nrpk.core]
+    ;[nrpk.spa]))
+
+(defn kee-start []
+      (k/start! {:routes         app-data/routes,
+                 :initial-db     app-data/initial-db,
+                 ;:screen         nrpk.core/screen-breakpoints
+                 ;:root-component [nrpk.spa/app-wrapper spa/route-table]
+                 :not-found      "/not-found",
+                 :hash-routing?  false}))
+
+(defn ^:dev/after-load reload! []
+      (js/console.log "loaded!")
+      (rf/clear-subscription-cache!)
+      (kee-start)
+      (rf/dispatch [:app/setup-handlers]))
+
+(defn init! []
+      (db/init! {:config app-data/booking-firebaseconfig})
+      (reload!)
+      (rf/dispatch [::rs/transition :main :e.restart]))
